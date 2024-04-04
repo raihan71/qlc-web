13dd9ce2d0fe337daa885ce1cc8b99f706f20863
Raihan Nismara <31585789+raihan71@users.noreply.github.com>
2024-04-04 10:49:28 +0700

    separated components, added owl carousel

---
+++ src/components/cardlist/cardlist.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardlistComponent } from './cardlist.component';
+
+describe('CardlistComponent', () => {
+  let component: CardlistComponent;
+  let fixture: ComponentFixture<CardlistComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CardlistComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CardlistComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/components/cardlist/cardlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cardlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './cardlist.component.html',
+  styleUrl: './cardlist.component.css'
+})
+export class CardlistComponent {
+
+}
